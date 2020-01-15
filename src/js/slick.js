cb2defaf018352fb627f2cdaf365eac277dd196b
ivandoroshenko <ivandoroshenko1988@gmail.com>
2020-01-15 13:51:31 +0200

    added slick.js

---
+++ src/js/slick.js
@@ -0,0 +1,36 @@
+let doc = document;
+let mainSlider = doc.querySelector('.main-slider'); 
+    
+    doc.addEventListener('DOMContentLoaded', function (event) {
+        $(mainSlider).slick({
+
+            dots: false,
+            arrows: true,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            speed: 1000,
+            fade: false,
+            cssEase: 'ease-in-out',
+            adaptiveHeight: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        
+            prevArrow: $('.prev'),
+            nextArrow: $('.next'),
+            responsive: [
+                {
+                    breakpoint: 1030,
+                    settings: {
+                        arrows: false
+        
+                    }
+                    
+                }
+            ]
+        });
+    });
+   
+    
+
+    $('.slick-slider').slick('refresh');
